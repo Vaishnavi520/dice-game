1f8e649433851485931be078e529ba4370f4e68e
Vaishnavi <125639025+Vaishnavi520@users.noreply.github.com>
2024-10-03 20:30:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var randomNumber1=Math.floor(Math.random()*6);
 randomNumber1=randomNumber1+1;
 
-var randomDice1="dice"+randomNumber1+".png"; //dice1.png
+var randomDice1="./dice"+randomNumber1+".png"; //dice1.png
 // var image1= "./images/"+ randomDice1; // ./images/.dice1.png;
 
 document.querySelector(".img1").setAttribute("src",randomDice1);
@@ -11,7 +11,7 @@ document.querySelector(".img1").setAttribute("src",randomDice1);
 var randomNumber2=Math.floor(Math.random()*6);
 randomNumber2=randomNumber2+1;
 
-var randomDice2="dice"+randomNumber2+".png"; //dice1.png
+var randomDice2="./dice"+randomNumber2+".png"; //dice1.png
 // var image2= "./images/"+ randomDice2; // ./images/.dice1.png;
 
 document.querySelector(".img2").setAttribute("src",randomDice2);
@@ -20,6 +20,6 @@ if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="Player 1 Wins";
 }else if(randomNumber1===randomNumber2){
     document.querySelector("h1").innerHTML="Draw";
-}else if(randomNumber1<randomNumber2)
+}else{
     document.querySelector("h1").innerHTML="Player 2 Wins";
 }
